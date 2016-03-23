06dd283c4d2959c623ea422640c42ebf397a7a8b
Nikki Kononov <nikitakononov93@gmail.com>
2016-03-23 16:03:22 +0200

    Init Continue
    Add themeConfiguration
    Add ngcloak to body element to make sure that the page is not displayed until it's fully loaded

--- JS/angular-module.js
+++ JS/angular-module.js
@@ -1,3 +1,18 @@
-(function() {
-  angular.module('webPage', []);
+(function (){
+  'use strict'
+
+  angular
+    .module('webApp', ['ngMaterial'])
+    .config(themeConfiguration);
+
+  function themeConfiguration($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue', {
+        'default': '500'
+      })
+      .accentPalette('red')
+      .warnPalette('deep-orange')
+      .backgroundPalette('grey');
+  };
+
 })();
