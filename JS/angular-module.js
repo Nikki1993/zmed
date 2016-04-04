60d001ac9f7e41c5672e9dd01eabc30799caff14
Nikki Kononov <nikitakononov93@gmail.com>
2016-04-04 18:46:03 +0300

    Broken Scrolling

--- JS/angular-module.js
+++ JS/angular-module.js
@@ -1,43 +1,44 @@
 (function() {
-  'use strict'
+    'use strict'
 
-  angular
-    .module('webApp', ['ngMaterial'])
-    .config(themeConfiguration)
-    .controller('mainController', mainController);
+    angular
+        .module('webApp', ['ngMaterial', 'duScroll'])
+        .config(themeConfiguration)
+        .controller('mainController', mainController);
 
     mainController.$inject = ['$mdSidenav'];
 
-  function themeConfiguration($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('blue', {
-        'default': '500'
-      })
-      .accentPalette('red')
-      .warnPalette('deep-orange')
-      .backgroundPalette('grey', {
-        'default': '100'
-      });
-  };
-
-  function mainController($mdSidenav) {
-    var vm = this;
-
-    vm.isSideNavOpen = false;
-
-    vm.openLeftMenu = function() {
-      if (vm.isSideNavOpen === false) {
-        $mdSidenav('left').toggle();
-        vm.isSideNavOpen = true;
-      };
+    function themeConfiguration($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue', {
+                'default': '500'
+            })
+            .accentPalette('red')
+            .warnPalette('deep-orange')
+            .backgroundPalette('grey', {
+                'default': '100'
+            });
     };
 
-    vm.closeLeftMenu = function() {
-      if (vm.isSideNavOpen === true) {
-        $mdSidenav('left').toggle();
+    function mainController($mdSidenav) {
+
+        var vm = this;
+
         vm.isSideNavOpen = false;
-      };
-    }
-  };
+
+        vm.openLeftMenu = function() {
+            if (vm.isSideNavOpen === false) {
+                $mdSidenav('left').toggle();
+                vm.isSideNavOpen = true;
+            };
+        };
+
+        vm.closeLeftMenu = function() {
+            if (vm.isSideNavOpen === true) {
+                $mdSidenav('left').toggle();
+                vm.isSideNavOpen = false;
+            };
+        }
+    };
 
 })();
