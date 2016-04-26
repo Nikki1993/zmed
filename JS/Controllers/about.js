057276f4a7a08737360ed085fb61653690cc74dd
Nikki Kononov <nikitakononov93@gmail.com>
2016-04-27 01:12:41 +0300

    System changes
    Make system count the amount of JSON items so that it repeats exactly the same amount instead of using ng-init with a need of modifying the value each time JSON file is edited.

--- JS/Controllers/about.js
+++ JS/Controllers/about.js
@@ -5,14 +5,24 @@
     .module('webApp')
     .controller('aboutCardController', aboutCardController);
 
-  aboutCardController.$inject = ['$translatePartialLoader', '$translate'];
+  aboutCardController.$inject = ['$translatePartialLoader', '$translate', 'JsonData'];
 
-  function aboutCardController($translatePartialLoader, $translate) {
+  function aboutCardController($translatePartialLoader, $translate, JsonData) {
 
     var vm = this;
 
+    vm.pathToJson = '../JSON/about-en.json';
+
     $translatePartialLoader.addPart('about');
     $translate.refresh();
+
+    JsonData.all(vm.pathToJson).then(function(response) {
+      vm.count = Object.keys(response.data.information).length;
+    });
+
+    vm.setObjectCount = function(n) {
+      return new Array(n);
+    }
 
     vm.getImageUrl = function(id) {
       return 'information.' + id + '.image';
