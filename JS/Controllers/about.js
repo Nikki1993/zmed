6e32dac44e6ae08ffba19d13e0436162f7c37f23
Nikki Kononov <nikitakononov93@gmail.com>
2016-04-27 20:24:03 +0300

    WIP part 2

--- JS/Controllers/about.js
+++ JS/Controllers/about.js
@@ -11,24 +11,31 @@
 
     var vm = this;
 
-    vm.name = ['.heading', '.content', '.image', '.imageAlt'];
+    vm.name = [];
 
     vm.pathToJson = '../JSON/about-en.json';
 
     JsonData.all(vm.pathToJson).then(function(response) {
       vm.count = JsonData.setObjectCount(Object.keys(response.data.information).length);
-      vm.namespace = Object.keys(response.data)[0] +'.';
+      vm.namespace = Object.keys(response.data)[0] + '.';
 
-      console.log(Object.keys(response.data)[0]);
-      console.log(Object.keys(response));
+      for (var property in Object.keys(response.data.information)[0]) {
+        if (Object.keys(response.data.information)[0].hasOwnProperty(property)) {
+          vm.name = Object.keys(response.data.information[property]);
+        };
+      };
     });
 
+    vm.getContent = function(id) {
+      return vm.namespace + id + '.' + vm.name[1];
+    }
+
     vm.getImageUrl = function(id) {
-      return vm.namespace + id + vm.name[2];
+      return vm.namespace + id + '.' + vm.name[2];
     };
 
     vm.getImageAlt = function(id) {
-      return vm.namespace + id + vm.name[3];
+      return vm.namespace + id + '.' + vm.name[3];
     };
   };
 })();
