8ee677a51903fb0afeb6a0f2bbdc3383a07b2296
Nikki Kononov <nikitakononov93@gmail.com>
2016-05-08 02:00:28 +0300

    finished gulp dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,21 @@ var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 var imagemin = require('gulp-imagemin');
+var del = require('del');
+
+// Clean dist folder
+
+gulp.task('default', function () {
+    
+});
+
+gulp.task('clean', function () {
+    return del.sync(['dist/**']);
+});
 
 // Optimize images
 
-gulp.task('default', function() {
+gulp.task('image', function() {
   return gulp.src('IMG/*')
     .pipe(imagemin())
     .pipe(gulp.dest('IMG'));
@@ -18,26 +29,27 @@ gulp.task('default', function() {
 gulp.task('js', function() {
   gulp.src(['JS/app.js', 'JS/**/*.js'])
     .pipe(sourcemaps.init())
-    .pipe(concat('webapp.js'))
+    .pipe(concat('app.js'))
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/JS/'));
 });
 
 // Copy paste files that do not require any additional work
 
 gulp.task('copy', function () {
   return gulp
-    .src(['index.html', 'JSON/*.json', 'IMG/*', 'CSS/styles.min*', 'TEMPLATES/*'], { base: './' })
-    .pipe(gulp.dest('dist/JS/'));
+    .src(['index.html', 'bower_components/**', 'JSON/**', 'IMG/**', 'CSS/styles.min*', 'TEMPLATES/**'], { base: './' })
+    .pipe(gulp.dest('dist/'));
 });
 
 // after all done, watch for JS changes and add them to webapp.js
 
 gulp.task('watch', ['js'], function() {
   gulp.watch('JS/**/*.js', ['js']);
-  gulp.watch('IMG/*.png', ['images']);
+  gulp.watch('IMG/*.png', ['image']);
+  gulp.watch(['index.html', 'bower_components/**', 'JSON/**', 'IMG/**', 'CSS/styles.min*', 'TEMPLATES/**'], ['copy']);
 });
 
-gulp.task('default', ['js', 'copy', 'watch']);
+gulp.task('default', ['clean', 'image', 'js', 'copy', 'watch']);
