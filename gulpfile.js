f6cc9e346015b28a68f6c08f9a5416ff5a5f5886
Nikki Kononov <nikitakononov93@gmail.com>
2016-05-08 00:17:06 +0300

    Node packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate')
 
 const imagemin = require('gulp-imagemin');
 
 gulp.task('default', () => {
-    return gulp.src('IMG/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('IMG'));
+  return gulp.src('IMG/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('IMG'));
 });
+
+gulp.task('js', function() {
+  gulp.src(['JS/app.js', 'JS/**/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('JS/'))
+});
+
+gulp.task('watch', ['js'], function() {
+  gulp.watch('JS/**/*.js', ['js'])
+})
+
+gulp.task('default', ['js', 'watch']);
