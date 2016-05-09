4b7f14ff8f34929ca43ca6645ea9b7708f10c59b
Nikki Kononov <nikitakononov93@gmail.com>
2016-05-09 16:28:52 +0300

    Rewrite gulpfile.js
    Include live browser start and reloading
    SCSS to CSS + uglify and live injection
    Use cache for images to improve process speed

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,73 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
-var imagemin = require('gulp-imagemin');
-var del = require('del');
 
-// Clean dist folder
-
-gulp.task('default', function () {
-    
+gulp.task('default', function(){
+  runSequence('clean:dist', ['copy', 'images', 'scss', 'angularjs', 'browserSync', 'watch']);
 });
 
-gulp.task('clean', function () {
-    return del.sync(['dist/**']);
+gulp.task('clean:dist', function() {
+  return del.sync(['dist']);
 });
 
-// Optimize images
+gulp.task('scss', function() {
+  return gulp.src('CSS/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('dist/CSS'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
-gulp.task('image', function() {
+gulp.task('images', function() {
   return gulp.src('IMG/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('IMG'));
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('dist/IMG'));
 });
 
-// Optimize Angularjs files, concat, uglify + preserving angularjs injections
-
-gulp.task('js', function() {
+gulp.task('angularjs', function() {
   gulp.src(['JS/app.js', 'JS/**/*.js'])
     .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/JS/'));
+    .pipe(gulp.dest('dist/JS'));
 });
 
-// Copy paste files that do not require any additional work
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    },
+    reloadDelay: 1500,
+    port: 8000
+  });
+});
+
+gulp.task('bs-reload', function () {
+  return browserSync.reload();
+});
 
 gulp.task('copy', function () {
   return gulp
-    .src(['index.html', 'bower_components/**', 'JSON/**', 'IMG/**', 'CSS/styles.min*', 'TEMPLATES/**'], { base: './' })
+    .src(['index.html', 'bower_components/**', 'JSON/**', 'TEMPLATES/**'], { base: './' })
     .pipe(gulp.dest('dist/'));
 });
 
-// after all done, watch for JS changes and add them to webapp.js
-
-gulp.task('watch', ['js'], function() {
-  gulp.watch('JS/**/*.js', ['js']);
-  gulp.watch('IMG/*.png', ['image']);
-  gulp.watch(['index.html', 'bower_components/**', 'JSON/**', 'IMG/**', 'CSS/styles.min*', 'TEMPLATES/**'], ['copy']);
+gulp.task('watch', ['browserSync', 'scss'], function() {
+  gulp.watch('CSS/*.scss', ['scss']);
+  gulp.watch(['index.html', 'bower_components/**', 'JSON/**', 'TEMPLATES/**'], ['copy']);
+  gulp.watch(['index.html', 'bower_components/**', 'JSON/**', 'TEMPLATES/**']).on('change', browserSync.reload);
 });
-
-gulp.task('default', ['clean', 'image', 'js', 'copy', 'watch']);
